8561f14465bb25144c2df275cb3c810bb0f4964f
Gabriel Coutinho <gabriel.souza@resourceit.com>
2018-06-07 13:57:23 -0300

    Inicialização Projeto CRUD - CLOUD

---
+++ ProjetoCRUD/BD/SqlConn.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+
+namespace ProjetoCRUD.BD
+{
+    public static class SqlConn
+    {
+        public static SqlConnection Abrir()
+        {
+            string str = System.Configuration.ConfigurationManager.ConnectionStrings["MinhaConn"].ConnectionString;
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            return conn;
+        }
+    }
+}
---
+++ ProjetoCRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoCRUD.MvcApplication" Language="C#" %>
